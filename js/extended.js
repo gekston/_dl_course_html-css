bf94ee56924aa77eb37a2376249a0101b89b9f4d
gekstone <lind999@ya.ru>
2015-09-21 15:46:06 +0300

    add
    dynamic creation left right top list btn
    load jquery from googl

--- js/extended.js
+++ js/extended.js
@@ -83,6 +83,7 @@ function link(){
   document.head.appendChild(script);
 }
 link();
+
 var container="<div class='wid-container'></div>"
 var expand="<div class='tool-wrapper'><div class='toolbar-exp'></div></div>"
 var stars =
@@ -98,27 +99,7 @@ var stars =
     "<span id='mark1'> Кількість голосів:</span>"+
     "</div>"
 
-// load css js
-function loadjscssfile(filename, filetype){
-    if (filetype=="js"){ //if filename is a external JavaScript file
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", filename)
-    }
-    else if (filetype=="css"){ //if filename is an external CSS file
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
-    }
-    if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref)
-}
-
-loadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js", "js") //dynamically load materialize.min.js
-loadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css", "css") ////dynamically load materialize.min.css
-//loadjscssfile("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css", "css") ////dynamically load bootstrap.min.css
-loadjscssfile("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css", "css") ////dynamically load font-awesome.min.css
+//
 
 // onload add div with list course
 window.onload=function() {
@@ -128,7 +109,17 @@ window.onload=function() {
     document.body.appendChild(d);
     i.className='slideout_inner';
     document.body.appendChild(i);
+/* create btn left rigt top list */
+    document.querySelector('table:nth-child(5) tr td:nth-child(1) a').innerHTML += '<i class="fa fa-arrow-left fa-2x"> </i>';
+    document.querySelector('table:nth-child(5) tr td:nth-child(3) a').innerHTML += '<i class="fa fa-arrow-right fa-2x"> </i>';
+    //document.getElementsByClassName("footer");
+    document.querySelector('.footer tr:nth-child(1) td:nth-child(1) a').innerHTML += '<i class="fa fa-arrow-left fa-2x"> </i>';
+    document.querySelector('.footer tr:nth-child(1) td:nth-child(2) a').innerHTML += '<i class="fa fa-arrow-left fa-rotate-90 fa-2x"> </i>';
+    document.querySelector('.footer tr:nth-child(1) td:nth-child(3) a').innerHTML += '<i class="fa fa-arrow-right fa-2x"> </i>';
+    document.querySelector('.footer tr:nth-child(2) td:nth-child(2) a').innerHTML += '<i class="fa fa-list-alt fa-2x"> </i>';
 }
+
+
 
 /**
  * Запуск дополнительных элементов при загрузке окна
